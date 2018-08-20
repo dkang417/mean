6ec33fd8abdf541a846e2016203fe1dc2dab5786
dkang417 <dkang417@gmail.com>
2018-08-20 11:37:36 -0400

    created mean app

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule, MatCardModule, MatCard , MatButtonModule} from '@angular/material';
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    FormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
