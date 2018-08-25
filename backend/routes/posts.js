adf65d9e8d6da8e1b948d0e8b27c92b9e4dee16c
dkang417 <dkang417@gmail.com>
2018-08-25 11:40:27 -0400

    adding image upload feature

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,10 +1,35 @@
 const express = require("express");
 
+// to upload image
+const multer = require('multer');
+const MIME_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+};
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIME_TYPE_MAP[file.mimetype];
+    let error = new Error('Invalid mime type');
+    if (isValid) {
+      error = null;
+    }
+    cb(error, 'backend/images');
+  },
+  filename: (rq, file, cb) => {
+    const name = file.originalname.toLowerCase().split('').join('-');
+    const ext = MIME_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext);
+  }
+});
+
+
+
 const Post = require("../models/post");
 
 const router = express.Router();
 
-router.post("", (req, res, next) => {
+router.post("", multer(storage).single('image'), (req, res, next) => {
   const post = new Post({
     title: req.body.title,
     content: req.body.content
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,33 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import {  FormGroup, FormControl, Validators } from '@angular/forms';
-import { PostsService } from '../posts.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import { PostsService } from '../posts.service';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-
 export class PostCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
-  isLoading = false;
   post: Post;
+  isLoading = false;
   form: FormGroup;
+  imagePreview: string;
   private mode = 'create';
   private postId: string;
 
-
-  constructor(public postsService: PostsService, public route: ActivatedRoute ) {}
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.form = new FormGroup({
-      'title': new FormControl(null, {
+      title: new FormControl(null, {
         validators: [Validators.required, Validators.minLength(3)]
       }),
-      'content': new FormControl(null, {validators: [Validators.required]})
+      content: new FormControl(null, { validators: [Validators.required] }),
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
+      })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
@@ -42,8 +50,8 @@ export class PostCreateComponent implements OnInit {
             content: postData.content
           };
           this.form.setValue({
-            'title': this.post.title,
-            'content': this.post.content
+            title: this.post.title,
+            content: this.post.content
           });
         });
       } else {
@@ -51,6 +59,17 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
