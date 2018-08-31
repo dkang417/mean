ab2cd260266ce0a822e9bd3c2d91c5260cad00c3
dkang417 <dkang417@gmail.com>
2018-08-31 10:45:17 -0400

    added controllers

---
+++ backend/controllers/posts.js
@@ -0,0 +1,29 @@
+const Post = require("../models/post");
+
+exports.createdPost = (req, res, next) => {
+  const url = req.protocol + '://' + req.get('host');
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    imagePath: url + '/images/' + req.file.filename,
+    creator: req.userData.userId
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added successfully",
+      post: {
+        ...createdPost,
+        id: createdPost._id,
+        // title: createdPost.title,
+        // content: createdPost.content,
+        // imagePath: createdPost.imagePath
+      }
+    });
+  })
+    .catch(error => {
+      res.status(500).json({
+        message: 'creating a post failed!'
+      });
+    });
+
+};
