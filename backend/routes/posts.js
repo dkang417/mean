ad5aeec8f9046f65dc40893ab9089a839fa89255
dkang417 <dkang417@gmail.com>
2018-08-31 10:26:17 -0400

    update error dialog

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -40,18 +40,24 @@ router.post(
       imagePath: url + '/images/' + req.file.filename,
       creator: req.userData.userId
   });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: "Post added successfully",
-      post: {
-        ...createdPost,
-        id: createdPost._id,
-        // title: createdPost.title,
-        // content: createdPost.content,
-        // imagePath: createdPost.imagePath
-      }
-    });
-  });
+    post.save().then(createdPost => {
+      res.status(201).json({
+        message: "Post added successfully",
+        post: {
+          ...createdPost,
+          id: createdPost._id,
+          // title: createdPost.title,
+          // content: createdPost.content,
+          // imagePath: createdPost.imagePath
+        }
+      });
+    })
+      .catch(error => {
+        res.status(500).json({
+          message: 'creating a post failed!'
+        });
+      });
+
 });
 
 router.put(
@@ -72,13 +78,19 @@ router.put(
     creator: req.userData.userId
   });
 
-  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post).then(result => {
-    if (result.nModified > 0) {
-      res.status(200).json({ message: "Update successful!" });
-    } else {
-      res.status(401).json({ message: 'not authorized' });
-    }
-  });
+    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post
+    ).then(result => {
+      if (result.nModified > 0) {
+        res.status(200).json({ message: "Update successful!" });
+      } else {
+        res.status(401).json({ message: 'not authorized' });
+      }
+    })
+      .catch(error => {
+        res.status(500).json({
+          message: 'couldnt update post'
+        });
+      });
 });
 
 router.get("", (req, res, next) => {
@@ -96,14 +108,19 @@ router.get("", (req, res, next) => {
     .then(documents => {
       fetchedPosts = documents;
       return Post.count();
-  })
-  .then(count => {
-    res.status(200).json({
-      message: 'Posts fetched successfully',
-      posts: fetchedPosts,
-      maxPosts: count
+    })
+    .then(count => {
+      res.status(200).json({
+        message: 'Posts fetched successfully',
+        posts: fetchedPosts,
+        maxPosts: count
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: 'fetching posts failed!'
+      });
     });
-  });
 });
 
 router.get("/:id", (req, res, next) => {
@@ -113,6 +130,10 @@ router.get("/:id", (req, res, next) => {
     } else {
       res.status(404).json({ message: "Post not found!" });
     }
+  }).catch(error => {
+    res.status(500).json({
+      message: 'fetching posts failed!'
+    });
   });
 });
 
@@ -123,6 +144,11 @@ router.delete("/:id", checkAuth, (req, res, next) => {
     } else {
       res.status(401).json({ message: 'not authorized' });
     }
+  }
+  ).catch(error => {
+    res.status(500).json({
+      message: 'fetching posts failed!'
+    });
   });
 });
 
